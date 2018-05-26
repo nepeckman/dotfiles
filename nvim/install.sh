90c769f266eda6148b53f94c3b75d68512b32f35
nepeckman <elijahpeckman@gmail.com>
2018-05-26 10:25:16 -0400

    install vim pluggins for nvim and oni

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,1 +1,3 @@
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+git clone https://github.com/scrooloose/nerdtree ~/.config/nvim/plugged/nerdtree
+git clone https://github.com/flazz/vim-colorschemes ~/.config/nvim/plugged/vim-colorschemes
